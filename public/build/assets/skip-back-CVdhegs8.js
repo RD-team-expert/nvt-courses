ebbeb23ad74eea42bcd303448f9b5f8ae39dfcf6
komys <komysobeh@gmail.com>
2025-10-08 20:49:05 +0300

    make 5

---
+++ public/build/assets/skip-back-CVdhegs8.js
@@ -0,0 +1,6 @@
+import{c as o}from"./Button.vue_vue_type_script_setup_true_lang-4lZsQt59.js";/**
+ * @license lucide-vue-next v0.468.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const e=o("SkipBackIcon",[["polygon",{points:"19 20 9 12 19 4 19 20",key:"o2sva"}],["line",{x1:"5",x2:"5",y1:"19",y2:"5",key:"1ocqjk"}]]);export{e as S};
